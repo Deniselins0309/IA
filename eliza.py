3949909870db05f044cfc8a1bd80bca455ace2d6
deniselins0309 <deniselins257@gmail.com>
2023-08-16 19:12:42 -0300

    alt

--- eliza.py
+++ eliza.py
@@ -1,10 +1,10 @@
 import streamlit as st
 import openai
 
-# Configure the OpenAI API Key (replace with your own key)
+# Configure the OpenAI API Key 
 openai.api_key = "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"
 
-def chatbot_interface(user_input):
+def generate_chat_response(user_input):
     response = openai.Completion.create(
         engine="davinci-codex",  # Use the davinci-codex engine
         prompt=user_input,
@@ -12,28 +12,33 @@ def chatbot_interface(user_input):
     )
     return response.choices[0].text.strip()
 
-st.title("Chatbot de Atendimento Eliza")
-st.markdown("Bem-vindo ao nosso chatbot de atendimento psicológico com nossa querida Eliza. Sinta-se à vontade para conversar sobre seus sentimentos e preocupações.")
+def main():
+    st.title("Chatbot de Atendimento Eliza")
+    st.markdown("Bem-vindo ao nosso chatbot de atendimento psicológico com nossa querida Eliza. Sinta-se à vontade para conversar sobre seus sentimentos e preocupações.")
 
-user_input = st.text_area("Digite sua mensagem aqui:")
+    user_input = st.text_area("Digite sua mensagem aqui:")
 
-if user_input:
-    bot_response = chatbot_interface(user_input)
-    st.text("Resposta:")
-    st.success(bot_response)
+    if user_input:
+        if st.button("Enviar"):
+            bot_response = generate_chat_response(user_input)
+            st.text("Resposta:")
+            st.success(bot_response)
 
-st.sidebar.title("Exemplos:")
-st.sidebar.write("Selecione um exemplo para iniciar a conversa:")
-examples = [
-    "Oi, estou me sentindo um pouco sobrecarregado hoje.",
-    "Como posso lidar com o estresse do trabalho?",
-    "Gostaria de aprender a relaxar."
-]
-selected_example = st.sidebar.selectbox("Exemplos:", examples)
+    st.sidebar.title("Exemplos:")
+    st.sidebar.write("Selecione um exemplo para iniciar a conversa:")
+    examples = [
+        "Oi, estou me sentindo um pouco sobrecarregado hoje.",
+        "Como posso lidar com o estresse do trabalho?",
+        "Gostaria de aprender a relaxar."
+    ]
+    selected_example = st.sidebar.selectbox("Exemplos:", examples)
 
-if st.sidebar.button("Iniciar Conversa"):
-    bot_response = chatbot_interface(selected_example)
-    st.text("Resposta:")
-    st.success(bot_response)
+    if st.sidebar.button("Iniciar Conversa"):
+        bot_response = generate_chat_response(selected_example)
+        st.text("Resposta:")
+        st.success(bot_response)
 
-st.write("Desenvolvido com ❤️ por [Denise Lins.. ]")
+    st.write("Desenvolvido com ❤️ por [Denise Lins.. ]")
+
+if __name__ == "__main__":
+    main()
